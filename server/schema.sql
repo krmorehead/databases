0af9bd1758d0679c0af50df9e20865b853d52834
krmorehead <krmorehead@gmail.com>
2015-12-18 20:04:44 -0800

    got the database to exist

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,47 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms(
+  room_name varchar(225),
+  messages_id int,
+  user_id int,
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users(
+  user_name varchar(225),
+  room_id int,
+  messages_id int,
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id)    
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message varchar(225),
+  user_id int,
+  room_id int,
+  cid integer,
+  time_stamp timestamp,
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+
+-- CREATE TABLE rooms_users(
+--   room_id int,
+--   user_id int,
+--   id int NOT NULL AUTO_INCREMENT,
+--   PRIMARY KEY(id),
+--   FOREIGN KEY(user_id) REFERENCES users(id),  
+--   FOREIGN KEY(room_id) REFERENCES rooms(id)  
+-- );
 
 
 
